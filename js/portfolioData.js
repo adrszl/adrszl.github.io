1580fb70e6f96d5f5f5f7fa990d64b59bd28365d
Adrian Szlegel <adrian95999@gmail.com>
2026-03-23 17:39:15 +0100

    portfolio data separated

---
+++ js/portfolioData.js
@@ -0,0 +1,75 @@
+// Portfolio data for carousel
+export const portfolioData = [
+    {
+        id: 1,
+        title: 'Website - 2019',
+        description: 'Personal website for Mr. Arkadiusz Trajdos',
+        image: 'img/project10.png',
+        tech: ['HTML', 'CSS', 'JS', 'PHP', 'Wordpress'],
+        projectLink: '404'
+    },
+    {
+        id: 2,
+        title: 'E-Commerce Website - 2020',
+        description: 'E-commerce website for PC Belfast',
+        image: 'img/project11.png',
+        tech: ['HTML', 'CSS', 'JS', 'PHP', 'Wordpress', 'WooCommerce'],
+        projectLink: 'https://www.pcbelfast.co.uk/'
+    },
+    {
+        id: 3,
+        title: 'Website - 2024',
+        description: 'Website built PIN Kultury with podcast player',
+        image: 'img/project16.png',
+        tech: ['HTML', 'CSS', 'JS', 'PHP', 'Wordpress'],
+        projectLink: '404'
+    },
+    {
+        id: 4,
+        title: 'Website - 2023',
+        description: 'Website built for Narimane and Karol, who were getting married',
+        image: 'img/project15.png',
+        tech: ['HTML', 'CSS', 'JS'],
+        projectLink: '404'
+    },
+    {
+        id: 5,
+        title: 'Website - 2023',
+        description: 'Website built for Compensum banking law office',
+        image: 'img/project14.png',
+        tech: ['HTML', 'CSS', 'JS', 'PHP', 'Wordpress'],
+        projectLink: '404'
+    },
+    {
+        id: 6,
+        title: 'E-Commerce Website - 2020',
+        description: 'E-commerce functionality for Mr. Arkadiusz Trajdos',
+        image: 'img/project12.png',
+        tech: ['HTML', 'CSS', 'JS', 'PHP', 'Wordpress', 'WooCommerce'],
+        projectLink: '404'
+    },
+    {
+        id: 7,
+        title: 'Magna Polonia Webpage - 2018 -> now',
+        description: 'Website for Magna Polonia academic corporation',
+        image: 'img/project13.png',
+        tech: ['HTML', 'CSS', 'JS', 'PHP', 'Wordpress'],
+        projectLink: 'https://magnapolonia.pl/'
+    },
+    {
+        id: 8,
+        title: 'Personal Portfolio Webpage - 2018',
+        description: 'Simple page intended to be "Web business card"',
+        image: 'img/project1.png',
+        tech: ['HTML', 'CSS'],
+        projectLink: 'https://codepen.io/adrszl/full/WaXoWw'
+    },
+    {
+        id: 9,
+        title: 'Tribute Page (Simple Landing Page) - 2018',
+        description: 'Simple page intended to be landing page with static content',
+        image: 'img/project2.png',
+        tech: ['HTML', 'CSS'],
+        projectLink: 'https://codepen.io/adrszl/full/EdjRPe/'
+    },
+];
